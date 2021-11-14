acae76ee0d0e2eba84050340dd9d04808a3d90c3
Ruben van Eldik <rubenvaneldik@protonmail.com>
2021-11-14 23:38:14 +0100

    chore: Remove unnecessary lines in nuxt.config.js

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,11 +1,7 @@
 export default {
-  // Disable server-side rendering: https://go.nuxtjs.dev/ssr-mode
   ssr: false,
-
-  // Target: https://go.nuxtjs.dev/config-target
   target: 'static',
-
-  // Global page headers: https://go.nuxtjs.dev/config-head
+  components: true,
   head: {
     title: 'Birds of Amsterdam',
     htmlAttrs: {
@@ -25,29 +21,8 @@ export default {
   env: {
     mapboxToken: process.env.MAPBOX_TOKEN
   },
-
-  // Global CSS: https://go.nuxtjs.dev/config-css
-  css: [
-  ],
-
-  // Plugins to run before rendering page: https://go.nuxtjs.dev/config-plugins
-  plugins: [
-  ],
-
-  // Auto import components: https://go.nuxtjs.dev/config-components
-  components: true,
-
-  // Modules for dev and build (recommended): https://go.nuxtjs.dev/config-modules
   buildModules: [
     // https://go.nuxtjs.dev/tailwindcss
     '@nuxtjs/tailwindcss',
   ],
-
-  // Modules: https://go.nuxtjs.dev/config-modules
-  modules: [
-  ],
-
-  // Build Configuration: https://go.nuxtjs.dev/config-build
-  build: {
-  }
 }
