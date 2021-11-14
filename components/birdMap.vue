55a790aac5cfaf868d4f18dd8ded627f72c29918
Ruben van Eldik <rubenvaneldik@protonmail.com>
2021-11-14 23:24:04 +0100

    feat: Add the map

--- components/birdMap.vue
+++ components/birdMap.vue
@@ -9,19 +9,70 @@
 import mapboxgl from 'mapbox-gl'
 
 export default {
+  data () {
+    return {
+      map: null,
+      unsubscribes: []
+    }
+  },
   mounted () {
-    // Set the Mapbox accestoken
-    mapboxgl.accessToken = process.env.mapboxToken
+    this.initializeMap()
+    this.addBirds()
+  },
+  beforeDestroy () {
+    // Unsubscribe the store watchers before destroying the component
+    this.unsubscribes.forEach(unsubscribe => unsubscribe())
+  },
+  methods: {
+    initializeMap () {
+      // Set the Mapbox accestoken
+      mapboxgl.accessToken = process.env.mapboxToken
 
-    // Initialize the Mapbox map
-    new mapboxgl.Map({
-      container: 'map',
-      style: 'mapbox://styles/mapbox/outdoors-v11',
-      zoom: 10,
-      center: [4.893333, 52.373056]
-    })
+      // Initialize the Mapbox map
+      this.map = new mapboxgl.Map({
+        container: 'map',
+        style: 'mapbox://styles/mapbox/outdoors-v11',
+        zoom: 10,
+        center: [4.893333, 52.373056]
+      })
+    },
+    async addBirds () {
+      // Get all birds
+      await this.$store.dispatch('birds/get')
 
-    this.$store.dispatch('birds/get')
+      // Add a layer for each species
+      this.$store.state.birds.species.forEach(species => {
+        const birds = this.$store.state.birds.data
+
+        this.map.addSource(species.name, {
+          'type': 'geojson',
+          'data': {
+              ...birds,
+              features: birds.features.filter(({ properties }) => properties.Vogel === species.name)
+          }
+        })
+
+        this.map.addLayer({
+          id: species.name,
+          type: 'circle',
+          source: species.name,
+          visible: species.show ? 'visible' : 'none',
+          paint: {
+            'circle-radius': 5,
+            'circle-color': species.color,
+            'circle-opacity': 0.7
+          }
+        })
+
+        this.unsubscribes.push(this.$store.subscribe((mutation, state) => {
+          const { name, show } = this.$store.state.birds.species.find(({ name }) => name === species.name)
+
+          if (mutation.type === 'birds/toggleSpecies' && mutation.payload === name) {
+            this.map.setLayoutProperty(name, 'visibility', show ? 'visible' : 'none')
+          }
+        }))
+      })
+    }
   }
 }
 </script>
